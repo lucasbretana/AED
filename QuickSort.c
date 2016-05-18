2633a28c3fde1e262ee615c326346cf7fe182622
Lucas E. Bretana <lucasbretana@gmail.com>
2016-05-18 18:49:32 -0300

    Teste atom

--- QuickSort.c
+++ QuickSort.c
@@ -6,23 +6,24 @@ int main(int argc, char const *argv[]) {
   int vet[] = {4,1,2,3,5,6,0};
 
 	quickSort(vet, 0, 7);
-	
 
   return 0;
 }
 
+/*
+  O que caracteriza o quickSort é a divisao em relação ao pivo, sendo
+  a esquerda os menores que o pivo
+  a direira os maiores que o pivo
+  A recursao para cada uma desses vetores
+*/
 void quickSort(int *vet, int left, int right){
-	int i, pivot = left, aux;
-	for( i=left+1 ; i<right ; i++ ){
-		aux = vet[pivot];
-		if(vet[i] < vet[pivot]){
-			for(int j = pivot+1 ; j < i ; j++){
-				vet[j-1] = vet[j];
-			}
-			vet[i] = aux;
-		}
-	}
+  if(left == right) return; // parada da recursão
+  // int pivo = (left + right) / 2;
+  int pivo = left;
+  int i;
+  for(i=right;i>left+1;i--){
+    if(){
 
-//	quickSort(vet, left-1, pivot);
-//	quickSort(vet, pivot, right+1);
+    }
+  }
 }
